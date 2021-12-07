dea18c03968b9d04ca30876791b53ed9a07ae9de
minsu <karosis28@gmail.com>
2021-12-07 10:49:06 +0900

    mybatis mapper interface 테스트 관련
    BoardMapperTest 테스트 코드 에서 @Autowired로 BoardMapper  빈 주입이 안됨

---
+++ src/test/java/me/minsu/springboard/mapper/BoardMapperTest.java
@@ -0,0 +1,24 @@
+package me.minsu.springboard.mapper;
+
+import me.minsu.springboard.domain.BoardDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardMapperTest {
+
+    @Autowired
+    private BoardMapper boardMapper;
+
+    @Test
+    public void testOfInsert() {
+        BoardDTO params = new BoardDTO();
+        params.setTitle("1번 게시글 제목");
+        params.setContent("1번 게시글 내용");
+        params.setWriter("테스터");
+
+        int result = boardMapper.insertBoard(params);
+        System.out.println("결과는 " + result + "입니다.");
+    }
+}
